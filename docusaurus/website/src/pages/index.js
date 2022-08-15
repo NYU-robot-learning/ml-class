5a2e00efaa34ae6b27a7b776e0705f80cae9f908
lerrel <lerrel.pinto@gmail.com>
2022-08-15 16:25:17 -0400

    Update index.js

--- docusaurus/website/src/pages/index.js
+++ docusaurus/website/src/pages/index.js
@@ -84,6 +84,9 @@ function Home() {
               <p>
               As data and computational resources become ever more abundant, the ability to leverage both to gain insights take on an increasingly important role in our civilization. “Machine Learning” is an umbrella term for the algorithms, tools and approaches that promise to harness data in such a way. This class is a survey course intended to give an overview of all major flavors of Machine Learning. Importantly, we will place a particular emphasis on understanding the key algorithms employed in different subfields of Machine Learning, instead of treating them as a black box. Overall, the goal of this course is to enable students to become comfortable with the material to a point where they can pursue the study of more advanced topics in Machine Learning as well as employing Machine Learning methods to solve scientific and applied problems. In other words, the purpose of this class is to find out if Machine Learning is for you or not - and if it is, to enable you to pursue it further with confidence and competence. 
               </p>
+              <p>
+              This class is modeled on previous offerings from Spring 2022 and Fall 2021. Building on the traditions of these previous offerings, this version of the class will place a larger emphasis on practical, hands-on experience with building ML algorithms. To broaded the scope of this offering, we will add lectures on self-supervised learning and reinforcement learning.
+              </p>
             </div>
           </div>
         </div>
